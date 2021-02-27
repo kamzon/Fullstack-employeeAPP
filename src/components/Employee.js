503193383fa999b9f84d27758a586fbb75648e70
ab <ab@DESKTOP-238QLG6>
2021-02-27 12:52:15 +0100

    Employee table connect to asp webAPI

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,17 +1,57 @@
 import react,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table } from "react-bootstrap";
 
 
 
 export class Employee extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={deps:[]}
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+    
+    refreshList(){
+        fetch('https://localhost:44319/api/employee').
+        then(Response => Response.json())
+        .then(data => {
+            this.setState({deps:data});
+
+        });
+    }
+
+
     render(){
+        const {deps} = this.state;
+
         return(
-            <div className = "m-5 d-flex justify-content-left">
-                <h3>
-                    this is the Employee page.
-                </h3>
-            </div>
+            <Table className="m-5" striped bordred hover size="sm">
+                <thead>
+                    <tr>
+                        <th>EmployeeID</th>
+                        <th>EmployeeName</th>
+                        <th>Department</th>
+                        <th>MailID</th>
+                        <th>Date of joinin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deps.map(emp=>
+                        <tr key = {emp.EmployeeID}> 
+                            <td>{emp.EmployeeID}</td>
+                            <td>{emp.EmployeeName}</td>
+                            <td>{emp.Department}</td>
+                            <td>{emp.MailID}</td>
+                            <td>{emp.DOJ}</td>
+                        </tr>
+                        )}
+                </tbody>
+            </Table>
+            
         )
     }
 }
